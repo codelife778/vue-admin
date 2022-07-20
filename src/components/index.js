22f02c609f62203304763e4d13695a7cd77429c9
shine john <1712420346qq@gmail.com>
2022-07-20 19:54:50 +0800

    Revert "主页完成"
    This reverts commit 88d068b7ff7355d14ae47b962e605edb34971ee8.

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,10 @@
 import BfCollapse from '@/components/BfCollapse/index'
 import OrderTable from '@/components/OrderTable/index'
-import BfProgress from '@/components/BfProgress/index'
 
 const bfComponent = {
     install: function(Vue) {
         Vue.component('BfCollapse', BfCollapse)
         Vue.component('OrderTable', OrderTable)
-        Vue.component('BfProgress', BfProgress)
     }
 }
 
