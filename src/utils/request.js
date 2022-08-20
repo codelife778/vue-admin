46ed467de1e66f9a1250b0bc7b53d9a1c3716786
shine john <1712420346qq@gmail.com>
2022-08-20 22:33:12 +0800

    #

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-    baseURL: 'http://v3.fgimax.vipnps.vip', // url = base url + request url
+    baseURL: 'https://5v936kf.nat.ipyingshe.com', // url = base url + request url
     // withCredentials: true, // send cookies when cross-domain requests
     timeout: 5000 // request timeout
 })
